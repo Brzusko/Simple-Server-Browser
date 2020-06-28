f3cf9aa3257282f9ded8c69fdfc699dcacdbcab1
Wojtek <wojciech.brzuszkiewicz@hotmail.com>
2020-06-28 22:11:50 +0200

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="">
+      <v-col
+        v-for="(server, i) in servers"
+        :key="i"
+        cols="12"
+      >
+      <server
+        :serverName="server.serverName"
+        :IP="server.IP"
+        :PORT="server.PORT"
+        :maxPlayers="server.maxPlayers"
+        :playerCount="server.playerCount"
+        :currentMap="server.currentMap"
+        :img="GetRandomIMG()"
+      />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import Server from './Server'
+  export default {
+    name: 'ServerList',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    components :{
+      Server
+    },
+    data: () => ({
+      ADDRESS: "http://127.0.0.1:7171/",
+      servers: [],
+      imgs:[
+        "https://i.imgur.com/3b8Mx2C.png",
+        "https://i.imgur.com/2IlrmBP.png",
+        "https://i.imgur.com/dMC3pTQ.png",
+        "https://i.imgur.com/BDyTSUS.png",
+        "https://i.imgur.com/9g1cJoF.png"
+      ]
+    }),
+    methods: {
+      async FetchServers() {
+        this.servers = []
+        let result = await this.$http(`${this.ADDRESS}getservers`);
+        if(result.data != undefined)
+          for(let i = 0; i < result.data.length; i++)
+            this.servers.push(result.data[i]);
+      },
+      GetRandomIMG(){
+        let index = Math.floor(Math.random() * this.imgs.length);
+        return this.imgs[index];
+      }
+    },
+    async mounted() {
+      await this.FetchServers();
+      setInterval(async() => {
+        await this.FetchServers();
+      }, 20000)
+    }
+  }
+</script>
---
+++ src/components/Server.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-card
+        class="mx-auto"
+        max-width="400"
+    >
+        <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="img"
+        >
+        <v-card-title>{{serverName}}</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pb-0"><b>Server Addres: {{IP}}:{{PORT}}</b></v-card-subtitle>
+
+        <v-card-text class="text--primary">
+        <div>Current map: {{currentMap}}</div>
+
+        <div>Slots {{playerCount}}/{{maxPlayers}}</div>
+        </v-card-text>
+
+        <v-card-actions>
+        <v-btn
+            color="orange"
+            text
+        >
+            Details
+        </v-btn>
+        </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'ServerList',
+    props: ["IP", "PORT", "currentMap", "maxPlayers", "playerCount", "serverName", "img"],
+    data: () => ({
+
+    }),
+  }
+</script>
